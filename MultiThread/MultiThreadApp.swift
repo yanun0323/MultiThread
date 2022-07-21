7ec521639c13245b8fa405b3bb5a2879fd50d40a
yanun <yanunyang@psp-power.com.tw>
2022-07-21 10:51:28 +0800

    Initial Commit

---
+++ MultiThread/MultiThreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiThreadApp.swift
+//  MultiThread
+//
+//  Created by YanunYang on 2022/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiThreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
